138961840cbbd4402d764bbaf19b618a955df263
Robius Sani <sani15-257@diu.edu.bd>
2021-11-05 22:38:45 +0600

    feat: added user type with user

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public const ADMIN = "admin";
+    public const USER = "user";
+
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +24,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'user_type'
     ];
 
     /**
